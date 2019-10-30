58921a83459186902e63a031641de777bd3848ef
xyl <ylxie@mmednet.com>
2019-10-30 16:56:49 +0800

    添加测试项目

---
+++ xyl_swagger/src/main/java/com/xyl/swagger/XylSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.xyl.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XylSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XylSwaggerApplication.class, args);
+	}
+
+}
---
+++ xyl_swagger/src/test/java/com/xyl/swagger/XylSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyl.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XylSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
