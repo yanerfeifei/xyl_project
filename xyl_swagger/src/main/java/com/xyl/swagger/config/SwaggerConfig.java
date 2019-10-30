508dc0267b21c8cb60502cc8723497c3ce74baa7
xyl <ylxie@mmednet.com>
2019-10-30 17:59:35 +0800

    添加swagger有关的代码

---
+++ xyl_swagger/src/main/java/com/xyl/swagger/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.xyl.swagger.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by meridian on 2019/10/30.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value(value="${spring.swagger2.enabled}")
+    private Boolean swaggerEnabled;
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerEnabled)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xyl.swagger"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+    /**
+     * 该套 API 说明，包含作者、简介、版本、host、服务URL
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("xyl接口文档")
+                .version("0.1")
+                .termsOfServiceUrl("localhost:8089/demo1/")
+                .description("xyl spring boot swagger")
+                .build();
+    }
+}
